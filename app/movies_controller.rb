9729ed1b4901f40554078aebd488f4118b8025a6
Andy Adams <andrewdcadams@gmail.com>
2020-10-02 13:44:05 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -105,7 +105,5 @@ def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  10.times do |i|
-    Movie.destroy_by(title: "Movie_#{i}")
-  end
+  Movie.destroy_all
 end
